fd400b84db4545b5fa265f2e3ac8e8d3a5edd506
Daniel Gallagher <danielbgallagher@gmail.com>
2020-10-29 11:51:26 -0400

    added config, migrations, models, and connected to DB in server.js

--- server.js
+++ server.js
@@ -34,3 +34,14 @@ app.get('/testing', (req, res) => {
 app.listen(process.env.PORT, () => {
     console.log("Server Running on port: " + process.env.PORT)
 })
+
+
+//Testing connection to database
+const { Sequelize } = require('sequelize')
+const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`)
+const db = require('./models')
+const review = require('./models/review')
+ db.sequelize.authenticate().then(()=>{ 
+       console.log("Great Sucess!")
+       db.sequelize.sync();
+ })
