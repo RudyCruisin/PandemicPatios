ee6f8d0edd1fcb2e4e21529fc04eb59c8ef692a4
Daniel Gallagher <danielbgallagher@gmail.com>
2020-11-02 11:14:18 -0500

    remove restaurant route

--- router/restaurantRoutes.js
+++ router/restaurantRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
+
+//Adds a business to the Restaurant table with fields from request body
 router.post('/add', async (req, res)=> {
     const { name, address, city, state, zipCode, phoneNumber } = req.body
 
@@ -17,4 +19,17 @@ router.post('/add', async (req, res)=> {
     res.send(newRestaurant)
 })
 
+//deletes business from DB 
+router.delete('/remove/:id', async (req, res)=> {
+    const id = req.params.id
+
+    const deleteRest = await db.Restaurant.destroy({
+        where: {
+            id
+        }
+    })
+    //add functionality that removes associated reviews 
+    res.send('Business Removed')
+
+})
 module.exports = router
