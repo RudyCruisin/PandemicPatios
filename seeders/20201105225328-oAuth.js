cb140dcf631f74dce592e6157cd100650ec1d78c
Daniel Gallagher <danielbgallagher@gmail.com>
2020-11-07 11:58:18 -0500

    idk

--- seeders/20201105225328-oAuth.js
+++ seeders/20201105225328-oAuth.js
@@ -2,15 +2,6 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
     await queryInterface.bulkInsert('oAuths',[{
       authStrat: "Twitter",     
       createdAt: new Date(),
